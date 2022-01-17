0a770e295a5bca28214f8f9747a1d4ade02434f7
Aron Zvi <aronzvi@gmail.com>
2022-01-17 18:41:52 -0300

    vaultProvider now in event message. incorrect exData property name

--- event-listener/config.js
+++ event-listener/config.js
@@ -32,44 +32,44 @@ module.exports = {
           addresses: ["0x0895196562C7868C5Be92459FaE7f877ED450452"], 
           abi: autoFarmV2Abi,
           listener: autoFarmV2Listener,
-          exData: {providerName: "autofarm"}
+          exData: {vaultProvider: "autofarm"}
         },
         {
           name: "AutoFarmTimelockController",
           addresses: ["0x28579ECA0a326E53340Edd93e18698D379876a04"], 
           abi: autoFarmTimelockControllerAbi,
           listener: autoFarmTimelockControllerListener,
-          exData: {providerName: "autofarm"}
+          exData: {vaultProvider: "autofarm"}
         },
         {
           name: "BunnyProxyAdmin",
           addresses: ["0x93c517e5191cc2a453b1564672ec1a7e3a58aa5d"], 
           abi: bunnyProxyAdminAbi,
           listener: bunnyProxyAdminListener,
-          exData: {providerName: "bunny"}
+          exData: {vaultProvider: "bunny"}
         },
         {
           name: "BunnyGnosisSafeProxy",
           addresses: ["0x14165A1b727AE8AF9d031C6F5faacA11E28A65BE"], 
           abi: bunnyGnosisSafeProxyAbi,
           listener: bunnyGnosisSafeProxyListener,
-          exData: {providerName: "bunny"}
+          exData: {vaultProvider: "bunny"}
         },
         {
           name: "BeefyTimelockController",
           addresses: ["0xA2E6391486670D2f1519461bcc915E4818aD1c9a"], 
           abi: beefyTimelockControllerAbi,
           listener: beefyTimelockControllerListener,
-          exData: {providerName: "beefy"}
+          exData: {vaultProvider: "beefy"}
         },
         {
           name: "RevaultTimelock",
           addresses: ["0x30ec49EF0243c00cF133a133e745C3957d4E28Bc"], 
           abi: revaultTimelockAbi,
           listener: revaultTimelockListener,
-          exData: {providerName: "revault"}
-        }
-        /*  
+          exData: {vaultProvider: "revault"}
+        },
+        /*
         {
           name: "RevaStakingPool",
           addresses: ["0x8B7b2a115201ACd7F95d874D6A9432FcEB9C466A"], 
@@ -89,7 +89,7 @@ module.exports = {
           addresses: ["0xe9e7cea3dedca5984780bafc599bd69add087d56"],
           abi: busdAbi,
           listener: busdListener,
-          exData: {az: "BUSD"}
+          exData: {vaultProvider: "BUSD"}
         }
         */
     ]
